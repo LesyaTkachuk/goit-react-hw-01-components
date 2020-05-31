bf1b78f975c0efa1d0d9450618578eb18412c8bf
Aleksandra Tkachuk <lesyalesya9876@gmail.com>
2020-05-31 15:34:06 +0300

    Updates

---
+++ precache-manifest.cde70ce9e088067ba54f1d3cae110204.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e9f5b73d33d418faf8a246927fde9e",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "300101db610fbb9d2225",
+    "url": "/goit-react-hw-01-components/static/css/main.de57b7f7.chunk.css"
+  },
+  {
+    "revision": "390183fa2a980628dba6",
+    "url": "/goit-react-hw-01-components/static/js/2.cd324757.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.cd324757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "300101db610fbb9d2225",
+    "url": "/goit-react-hw-01-components/static/js/main.b3ec3035.chunk.js"
+  },
+  {
+    "revision": "cf184cad0926235fce4a",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.f8c9733c.js"
+  }
+]);
